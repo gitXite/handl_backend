774b06b40fac9d0d6b65ac86b3d9329e2f1ccd53
Daniel <Daniel.halaas@gmail.com>
2025-02-18 07:31:54 +0100

    Create config/passport.js

---
+++ src/config/passport.js
@@ -0,0 +1,45 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const pool = require('../db/db');
+
+// Passport local strategy
+passport.use(
+    new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+        try {
+            console.log('Login attempt for:', email);
+            const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+            const user = result.rows[0];
+
+            if (!user) {
+                console.log('User not found');
+                return done(null, false, { message: 'User not found' });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                console.log('Incorrect password');
+                return done(null, false, { message: 'Incorrect password' });
+            }
+
+            console.log('Login successful', user.name);
+            return done(null, user);
+        } catch (error) {
+            return done(error);
+        }
+    })
+);
+
+// Store user ID in session
+passport.serializeUser((user, done) => done(null, user.id));
+
+// Retrieve user from ID
+passport.deserializeUser(async (id, done) => {
+    try {
+        const result = await pool.query('SELECT * FROM users WHERE id =$1', [id]);
+        const user = result.rows[0];
+        done(null, user);
+    } catch (error) {
+        done(error);
+    }
+});
