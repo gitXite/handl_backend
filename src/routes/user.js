2ab726b5c2ff8de0e8866a43107e0f11c80e9c2b
Daniel <Daniel.halaas@gmail.com>
2025-02-19 20:46:43 +0100

    res.status(200)

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,7 +6,7 @@ router.get('/profile', (req, res) => {
     if (!req.isAuthenticated()) {
         return res.status(401).json({message: 'Not logged in'});
     }
-    res.json({message: 'Welcome to your profile', user: req.user});
+    res.status(200).json({message: 'Welcome to your profile', user: req.user});
 });
 
 
