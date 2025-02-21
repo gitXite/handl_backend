35622b65f61946fbac23e41968588370e4a10128
Daniel <Daniel.halaas@gmail.com>
2025-02-21 20:27:12 +0100

    Update user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,6 @@
-const express = require('express');
 const router = express.Router();
+const authMiddleware = require('../middleware/authMiddleware');
+const authController = require('../controllers/authController');
 
 
 // Profile api route
