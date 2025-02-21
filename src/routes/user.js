e807c5c226b14419d9699887e6e01be0d8143dff
Daniel <Daniel.halaas@gmail.com>
2025-02-21 20:29:11 +0100

    Update user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 const authController = require('../controllers/authController');
