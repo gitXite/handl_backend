30329aac67ee2cb823fee49b4e51b7a82d67add8
Daniel <Daniel.halaas@gmail.com>
2025-02-21 20:04:21 +0100

    Finished profile api route

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,12 +3,7 @@ const router = express.Router();
 
 
 // Profile api route
-router.get('/profile', (req, res) => {
-    if (!req.isAuthenticated()) {
-        return res.status(401).json({message: 'Not logged in'});
-    }
-    res.status(200).json({message: 'Welcome to your profile', user: req.user});
-});
+router.get('/profile', authController.getProfile);
 
 
 module.exports = router;
