0d5dde24ddd67626ba0414d48f783eb23b74bde5
Daniel <Daniel.halaas@gmail.com>
2025-02-21 20:02:01 +0100

    Logout controller

--- src/controllers/sessionController.js
+++ src/controllers/sessionController.js
@@ -11,7 +11,25 @@ const checkSession = (req, res) => {
     }
 };
 
+// Logout controller
+const logoutUser = (req, res) => {
+    if (!req.isAuthenticated()) {
+        return res.status(400).json({ message: 'No user logged in' });
+    }
+
+    req.session.destroy((err) => {
+        if (err) {
+            return res.status(500).json({ message: 'Error destroying session' });
+        }
+
+        res.clearCookie('connect.sid', { path: '/' });
+        console.log('Logged out successfully');
+        res.status(200).json({ message: 'Logged out successfully' });
+    });
+};
+
 
 module.exports = {
     checkSession,
+    logoutUser,
 }
