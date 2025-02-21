293c52e1527141706cdff981ed62d8758fa36e12
Daniel <Daniel.halaas@gmail.com>
2025-02-21 20:22:47 +0100

    Update user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 
 // Profile api route
-router.get('/profile', authController.getProfile);
+router.get('/profile', authMiddleware.isAuthenticated, authController.getProfile);
 
 
 module.exports = router;
