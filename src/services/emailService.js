448a0c232076648b2fd186f82540cdbef2216b27
Daniel <Daniel.halaas@gmail.com>
2025-02-22 13:49:07 +0100

    Used ethereal to test email confirmation

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -5,22 +5,38 @@ const pool = require('../config/db');
 //
 // Confirmation email
 const sendConfirmationEmail = async (userEmail, token) => {
-    const transporter = nodemailer.createTransport({
+    let transporter;
+    if (process.env.NODE_ENV === 'production') {
+        transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
         },
     });
-
+    } else {
+        const testAccount = await nodemailer.createTestAccount();
+        transporter = nodemailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: testAccount.user,
+                pass: testAccount.pass,
+            },
+        });
+    }
+    
     const mailOptions = {
-        from: process.env.EMAIL_USER,
+        from: process.env.EMAIL_USER || 'test@ethereal.email',
         to: userEmail,
         subject: 'Confirm your Email',
         text: `Welcome to HANDL!\n\nClick the link to confirm your email: https://handl.dev/auth/confirm-email?token=${token}\nIf you encounter any problems, please let us know at support@handl.dev\n\nRegards, Daniel\nHANDL`,
     };
 
-    await transporter.sendMail(mailOptions);
+    const info = await transporter.sendMail(mailOptions);
+    if (process.env.NODE_ENV !== 'production') {
+        console.log('Test email preview URL:', nodemailer.getTestMessangerUrl(info));
+    }
 };
 
 
