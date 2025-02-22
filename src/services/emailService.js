c01ca1d127e00c6c3d479e23226edefdd5fb51a6
Daniel <Daniel.halaas@gmail.com>
2025-02-22 11:20:38 +0100

    Update emailService.js

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -10,5 +10,25 @@ async function sendConfirmationEmail(userEmail, userId) {
 
     await pool.query('UPDATE users SET email_token = $1 WHERE id = $2', [token, userId]);
 
-    //
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
+
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: userEmail,
+        subject: 'Confirm your Email',
+        text: `Welcome to HANDL!\n\nClick the link to confirm your email: https://handl.dev/auth/confirm-email?token=${token}\nIf you encounter any problems, please let us know at support@handl.dev\n\nRegards, Daniel\nHANDL`,
+    };
+
+    await transporter.sendMail(mailOptions);
+}
+
+
+module.exports = {
+    sendConfirmationEmail,
 }
