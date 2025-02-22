15ec7f8724fc8c59fbd3a3402ce0dd1a57ed97d2
Daniel <Daniel.halaas@gmail.com>
2025-02-22 14:44:05 +0100

    Controller for contact page

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -1,4 +1,18 @@
 const emailService = require('../services/emailService');
 
+
 // Controller functions for contact page
-// 
+const contactEmail = async (req, res) => {
+    const { name, email, subject, message } = req.body;
+    if (!name || !email || !subject || !message) {
+        res.status(400).json({ message: 'All fields must be filled out' });
+    }
+
+    await emailService.sendContactEmail(name, email, subject, message);
+    res.status(200).json({ message: 'Contact form submitted' });
+};
+
+
+module.exports = {
+    contactEmail,
+};
