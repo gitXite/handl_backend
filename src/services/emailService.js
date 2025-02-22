6b0c7c4aa1efe1f0f725f0dc1cdb03b18772a63c
Daniel <Daniel.halaas@gmail.com>
2025-02-22 11:27:07 +0100

    Update emailService.js

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -5,11 +5,7 @@ const pool = require('../config/db');
 // Email service functions
 //
 // Confirmation email
-async function sendConfirmationEmail(userEmail, userId) {
-    const token = crypto.randomBytes(32).toString('hex');
-
-    await pool.query('UPDATE users SET email_token = $1 WHERE id = $2', [token, userId]);
-
+async function sendConfirmationEmail(token) {
     const transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
