b4eda5ac6cb8f12f2c76589628bdf613c821bbf0
gitXite <daniel.halaas@gmail.com>
2025-02-25 15:18:06 +0100

    Small changes

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 5000;
 
 // CORS configuration
 app.use(cors({
-    origin: 'http://127.0.0.1:3000',
+    origin: 'http://localhost:3000',
     credentials: true,
     preflightContinue: false,
 }));
