9e6858ad9112e84a0e9849d1ed4677b9abccc009
Daniel <Daniel.halaas@gmail.com>
2025-03-07 17:24:36 +0100

    Create sse.js for realtime updates

---
+++ src/routes/sse.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const { sseHandler } = require('../controllers/sseController');
+
+
+router.get('/', sseHandler);
+
+
+module.exports = router;
