1fe5e57b5040e0b72ebd1301ce0552cd51aa69dd
Daniel <Daniel.halaas@gmail.com>
2025-03-07 18:09:13 +0100

    Boilerplate for broadcasting updates to lists

--- src/services/listService.js
+++ src/services/listService.js
@@ -1,1 +1,13 @@
+const { broadcastEvent } = require('../controllers/sseController');
 
+
+const updateShoppingList = (listId, updates) => {
+    // update database here
+    
+    const updatedList = { id: listId, ...updates };
+    broadcastEvent('list-updated', updatedList);
+    return updatedList;
+};
+
+
+module.exports = { updateShoppingList };
