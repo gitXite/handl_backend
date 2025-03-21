37d450253f6b1d4340884e636d17643324d77e20
Daniel <Daniel.halaas@gmail.com>
2025-03-21 18:34:45 +0100

    Update sseService.js

--- src/services/sseService.js
+++ src/services/sseService.js
@@ -1,19 +1,4 @@
 const pool = require('../config/db');
 
 
-const getSharedListsForUser = async (userId) => {
-    try {
-        const { rows } = await pool.query(
-            'SELECT list_id FROM shared_lists WHERE user_id = $1',
-            [userId]
-        );
 
-        return rows.map(row => row.list_id);
-    } catch (error) {
-        console.error("Error retrieving shared lists for user:", error);
-        throw error;
-    }
-};
-
-
-module.exports = { getSharedListsForUser }
