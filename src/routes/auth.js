5aa5a0cdd8956ec0a661f8b401dc5f82ac54755e
Daniel <Daniel.halaas@gmail.com>
2025-03-21 20:28:29 +0100

    Comment

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,19 +4,15 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const sessionController = require('../controllers/sessionController');
 
-
-// Register api route
+// Endpoint: /api/auth/
 router.post('/register', authController.registerUser);
-// Confirm email api route
+
 router.get('/confirm-email', authController.confirmEmail);
 
-// Login api route
 router.post('/login', authController.loginUser);
 
-// Session api route
 router.get('/session', sessionController.checkSession);
 
-// Logout api route
 router.post('/logout', sessionController.logoutUser);
 
 
